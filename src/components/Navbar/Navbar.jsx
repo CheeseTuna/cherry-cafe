b53285bf92ddd16760431795412b97fde4ab6f48
CheeseTuna <cisefm123@gmail.com>
2024-09-18 14:28:35 +0100

    Failed to compile react icons and images

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
 
-import './Navbar.css';
+import images from "../../constants/images";
+import "./Navbar.css";
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+const Navbar = () => <nav className="app__navbar">Navbar</nav>;
 
 export default Navbar;
